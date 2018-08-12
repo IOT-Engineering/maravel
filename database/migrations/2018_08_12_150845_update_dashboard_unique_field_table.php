2cf0150e12517b399693f8489fa1dd99f8c3160e
dacalin-mac <casale.candoit@gmail.com>
2018-08-12 17:16:08 +0200

    .

--- database/migrations/2018_08_12_150845_update_dashboard_unique_field_table.php
+++ database/migrations/2018_08_12_150845_update_dashboard_unique_field_table.php
@@ -15,7 +15,7 @@ class UpdateDashboardUniqueFieldTable extends Migration
     {
         Schema::table('dashboard', function(Blueprint $table)
         {
-            $table->dropUnique('controller_function');
+            $table->dropUnique(['controller_function']);
             $table->unique(['user_id','controller', 'function']);
         });
     }
@@ -30,7 +30,7 @@ class UpdateDashboardUniqueFieldTable extends Migration
         Schema::table('dashboard', function(Blueprint $table)
         {
             $table->dropUnique(['user_id_controller_function']);
-            $table->unique(['controller', 'function']);
+            //$table->unique(['controller', 'function']);
         });
     }
 }

a9138b1306c5da11a33614622344f49eec4f7590
miky <miky@mail.com>
2018-09-25 12:33:59 +0200

    artisan command

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Modules\Ftp\Http\Controllers\FtpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('upload-check', function () {
+    FtpController::uploadCheck();
+})->describe('Check the uploads of ftp users');
